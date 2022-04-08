eda1f37df458f4d31645bd25445954048636be65
Jesus Redondo <jesus.redondo@ft.com>
2022-04-08 07:54:10 +0200

    Add Unit Test for resolveUrlAndHost

--- __tests__/main.spec.js
+++ __tests__/main.spec.js
@@ -1,33 +1,36 @@
-const signedFetch = require('../main');
-const https = require('https');
+const rewire = require('rewire');
+const util = require('util');
+const dns = require('dns');
 
-const keepAliveAgent = new https.Agent({ keepAlive: true });
-const DEFAULTS = {
-	query: { match_all: {} },
-	from: 0,
-	size: 10,
-	sort: { publishedDate: 'desc' },
-	_source: true
-};
+let mainModule;
+beforeEach(() => {
+	jest.spyOn(util, 'promisify').mockImplementation((a) => a);
+	jest.spyOn(dns, 'resolveCname').mockResolvedValue(['host1.com', 'host2.com']);
+	mainModule = rewire('../main');
+});
 
-test('Verify the request is signed sucessfully', async () => {
-	const body = JSON.stringify(DEFAULTS);
-	const timeout = 3000;
+afterEach(() => {
+	jest.restoreAllMocks();
+});
 
-	const result = await signedFetch(
-		'https://next-elasticsearch-v7.gslb.ft.com/content/_search',
-		{
-			body,
-			agent: keepAliveAgent,
-			timeout,
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		}
-	);
-
-	expect(result.ok).toBe(true);
-	expect(result.status).toBe(200);
-	expect(result.statusText).toBe('OK');
+describe('resolveUrlAndHost', () => {
+	test(`the url host matchs /\.es\.amazonaws\.com$/ and it returned without modifications`, async () => {
+		const url = 'http://ft.es.amazonaws.com/mypath';
+		const result = await mainModule.__get__('resolveUrlAndHost')(url);
+		expect(result).toBe(url);
+	});
+	test(`process.env.AWS_SIGNED_FETCH_DISABLE_DNS_RESOLUTION=true and the url is returned witout modifications`, async () => {
+		const url = 'http://whatever.com/mypath';
+        const env = mainModule.__get__('process.env');
+        env.AWS_SIGNED_FETCH_DISABLE_DNS_RESOLUTION = true;
+		const result = await mainModule.__get__('resolveUrlAndHost')(url);
+		expect(result).toBe(url);
+		delete env.AWS_SIGNED_FETCH_DISABLE_DNS_RESOLUTION;
+	});
+	test(`the url doesn't match and process.env.AWS_SIGNED_FETCH_DISABLE_DNS_RESOLUTION=false, 
+	resolveCname is called and the first host returned is assigned to the url`, async () => {
+		const url = 'http://whatever.com/mypath';
+		const result = await mainModule.__get__('resolveUrlAndHost')(url);
+		expect(result).toBe('http://host1.com/mypath');
+	});
 });
---
+++ __tests__/regression.spec.js
@@ -0,0 +1,35 @@
+const signedFetch = require('../main');
+const https = require('https');
+
+const keepAliveAgent = new https.Agent({ keepAlive: true });
+const DEFAULTS = {
+	query: { match_all: {} },
+	from: 0,
+	size: 10,
+	sort: { publishedDate: 'desc' },
+	_source: true
+};
+
+
+test('Verify the request is signed sucessfully', async () => {
+	const body = JSON.stringify(DEFAULTS);
+	const timeout = 3000;
+
+	const result = await signedFetch(
+		'https://next-elasticsearch-v7.gslb.ft.com/content/_search',
+		{
+			body,
+			agent: keepAliveAgent,
+			timeout,
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}
+	);
+
+	expect(result.ok).toBe(true);
+	expect(result.status).toBe(200);
+	expect(result.statusText).toBe('OK');
+});
+
